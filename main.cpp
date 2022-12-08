7a0ca1a468e4a892e4424dce7237752e67f5b8a0
benedevrepo <benedikt.binger@gmail.com>
2022-12-08 20:14:28 +0100

    Started spring support

--- main.cpp
+++ main.cpp
@@ -31,8 +31,10 @@ float calcArea(const Triangle& tri) {
 }
 
 bmath::vec2 dirLinePoint(const bmath::vec2& l0, const bmath::vec2& l1, const bmath::vec2& p) {
-	const bmath::vec2 ldir = l1 - l0;
-	return p - (l0 + ldir * ldir.dot(p - l0));
+	const bmath::vec2 ldir = l1 - l0; // direction l0 -> l1
+	const bmath::vec2 lP = l0 + ldir * ldir.dot(p - l0); // point on line, that is closest to p
+	const bmath::vec2 dir = p - (lP);
+	return dir * calcArea({l0, l1, p}) * 2 / dir.mag(); // rescale to match cross product magnitude
 }
 
 struct TriangleConstraint : Constraint {
@@ -43,7 +45,7 @@ public:
 	inline TriangleConstraint(Triangle& target): target(target), area(calcArea(target)) { }
 	inline TriangleConstraint(Triangle& target, const float area): target(target), area(area) { }
 	virtual inline void solve(const float dt) {
-		const float invMass[3]{ 0.f, 1.f, 1.f };
+		const float invMass[3]{ 0.f, 0.f, 1.f };
 
 		const float currentArea = calcArea(target);
 
@@ -51,9 +53,9 @@ public:
 		bmath::vec2 gradient1 = dirLinePoint(target.p2(), target.p0(), target.p1());
 		bmath::vec2 gradient2 = dirLinePoint(target.p0(), target.p1(), target.p2());
 
-		gradient0 *= currentArea * 2 / gradient0.mag();
-		gradient1 *= currentArea * 2 / gradient1.mag();
-		gradient2 *= currentArea * 2 / gradient2.mag();
+		// gradient0 *= currentArea * 2 / gradient0.mag();
+		// gradient1 *= currentArea * 2 / gradient1.mag();
+		// gradient2 *= currentArea * 2 / gradient2.mag();
 
 		float w = 0.f;
 		w += invMass[0] * gradient0.magSquared();
@@ -76,6 +78,32 @@ public:
 };
 
 
+struct Spring {
+private:
+	bmath::vec2& p0;
+	bmath::vec2& p1;
+	const float m0, m1;
+	const float restLength;
+	const float strength;
+public:
+	inline Spring(bmath::vec2& p0, bmath::vec2& p1, const float strength, const float m0=1, const float m1=1):
+		p0(p0), p1(p1), restLength((p1 - p0).mag()), strength(strength), m0(m0), m1(m1) {}
+	// inline Spring(bmath::vec2& p0, bmath::vec2& p1, const float strength, const float restLength):
+	// 	p0(p0), p1(p1), restLength(restLength), strength(strength) {}
+	inline void update(const float dt) {
+		// const float invMass[2] { 1.f / m0, 1.f / m1};
+		const float invMass[2] { 1.f, 1.f};
+		const bmath::vec2 p0p1 = p1 - p0;
+		const bmath::vec2 dir01 = p0p1 / p0p1.mag();
+		const float currentLength = p0p1.mag();
+		const float deviation = currentLength - restLength;
+		float f = deviation * strength;
+		p0 += f * dir01 * dt * invMass[0];
+		p1 -= f * dir01 * dt * invMass[1];
+	}
+};
+
+
 int main() {
 	GDIWindow win(800, 800);
 
@@ -92,6 +120,12 @@ int main() {
 	};
 
 	TriangleConstraint c0(t0);
+
+	// Spring s0(t0.p0(), t0.p1(), 1.f, 1.f / 0.f, 1.f);
+	Spring s0(t0.p0(), t0.p1(), 1.f, 1000000000.f, 1000000000.f);
+	Spring s1(t0.p1(), t0.p2(), 1.f, 1000000000.f);
+	// Spring s2(t0.p2(), t0.p0(), 1.f, 0.f, 1.f / 0.f);
+	Spring s2(t0.p2(), t0.p0(), 1.f, 0.f, 1000000000.f);
 
 	// t0.p0()[0] += .3;
 
@@ -113,6 +147,11 @@ int main() {
 
 		// t0.p0()[0] = 2.f + 2.f * t;
 		t0.p0()[0] = 2.f + fabsf(2.f * t - 1.f);
+
+
+		s0.update(dt);
+		s1.update(dt);
+		s2.update(dt);
 
 		c0.solve(dt);
 
