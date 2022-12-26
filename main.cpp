0d27ab9be6c816c873697884bd95d185fc3b6881
Bene <benedikt.binger@gmail.com>
2022-12-26 04:36:02 +0100

    Updated BWindow Version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdint>
 #include <cmath>
+#include <vector>
 // #include <thread>
 // #include <mutex>
 
@@ -9,121 +10,25 @@
 #include "BMath/vector.h"
 
 #include "PointMass.hpp"
+#include "Spring.hpp"
+#include "LineConstraint.hpp"
+#include "TriangleConstraint.hpp"
 
 #include "windows.h"
 
 
-struct Constraint {
-	virtual ~Constraint() = default;
-	virtual inline void solve(const float dt) = 0;
-};
+GDIWindow* gwin;
 
-struct Triangle {
-	// bmath::vec2 p[3]{};
-	PointMass* p[3];
-	inline const bmath::vec2& p0() const { return p[0]->pos; }
-	inline const bmath::vec2& p1() const { return p[1]->pos; }
-	inline const bmath::vec2& p2() const { return p[2]->pos; }
-	inline bmath::vec2& p0() { return p[0]->pos; }
-	inline bmath::vec2& p1() { return p[1]->pos; }
-	inline bmath::vec2& p2() { return p[2]->pos; }
-};
+std::vector<PointMass> points;
+std::vector<LineConstraint> lines;
+std::vector<Spring> springs;
+std::vector<TriangleConstraint> triangles;
 
-// float calcArea(const Triangle& tri) {
-float calcArea(const bmath::vec2& p0, const bmath::vec2& p1, const bmath::vec2& p2) {
-	bmath::vec2 tmp(p1 - p0);
-	std::swap(tmp[0], tmp[1]);
-	tmp[1] *= -1;
-	const bool positive = tmp.dot(p2 - p0) < 0;
-	return (p1 - p0).cross(p2 - p0) * .5f;// * (positive ?: -1);
-}
-
-bmath::vec2 dirLinePoint(const bmath::vec2& l0, const bmath::vec2& l1, const bmath::vec2& p) {
-	const bmath::vec2 ldir = l1 - l0; // direction l0 -> l1
-	const bmath::vec2 lP = l0 + ldir * ldir.dot(p - l0); // point on line, that is closest to p
-	const bmath::vec2 dir = p - (lP);
-	return dir * calcArea(l0, l1, p) * 2 / dir.mag(); // rescale to match cross product magnitude
-}
-
-struct TriangleConstraint : Constraint {
-private:
-	Triangle target;
-	const float area;
-public:
-	inline TriangleConstraint(Triangle target): target(target), area(calcArea(target.p0(), target.p1(), target.p2())) { }
-	inline TriangleConstraint(Triangle target, const float area): target(target), area(area) { }
-	virtual inline void solve(const float dt) {
-		// const float invMass[3]{ 1.f, 1.f, 1.f };
-		const float invMass[3] {
-			1.f / target.p[0]->mass,
-			1.f / target.p[1]->mass,
-			1.f / target.p[2]->mass
-		};
-
-		const float currentArea = calcArea(target.p0(), target.p1(), target.p2());
-
-		bmath::vec2 gradient0 = dirLinePoint(target.p1(), target.p2(), target.p0());
-		bmath::vec2 gradient1 = dirLinePoint(target.p2(), target.p0(), target.p1());
-		bmath::vec2 gradient2 = dirLinePoint(target.p0(), target.p1(), target.p2());
-
-		// gradient0 *= currentArea * 2 / gradient0.mag();
-		// gradient1 *= currentArea * 2 / gradient1.mag();
-		// gradient2 *= currentArea * 2 / gradient2.mag();
-
-		float w = 0.f;
-		w += invMass[0] * gradient0.magSquared();
-		w += invMass[1] * gradient1.magSquared();
-		w += invMass[2] * gradient2.magSquared();
-
-		const float compliance = 5.f;
-		const float c = currentArea - area; // constraint value
-		const float alpha = compliance / (dt * dt);
-		const float lambda = -(2 * c) / (w + alpha);
-		// target.p0() += gradient0 * s;
-		std::cout << "Error: " << c << "  after: ";
-
-		target.p[0]->pos += gradient0 * lambda * invMass[0];
-		target.p[1]->pos += gradient1 * lambda * invMass[1];
-		target.p[2]->pos += gradient2 * lambda * invMass[2];
-
-		// target.p[0].prevPos += gradient0 * lambda * invMass[0];
-		// target.p[1].prevPos += gradient1 * lambda * invMass[1];
-		// target.p[2].prevPos += gradient2 * lambda * invMass[2];
-
-		std::cout << (calcArea(target.p0(), target.p1(), target.p2()) - area) << "\n";
-	}
-};
-
-
-struct Spring {
-private:
-	// bmath::vec2& p0;
-	// bmath::vec2& p1;
-	PointMass& p0;
-	PointMass& p1;
-	const float restLength;
-	const float strength;
-public:
-	inline Spring(PointMass& p0, PointMass& p1, const float strength):
-		p0(p0), p1(p1), restLength((p1.pos - p0.pos).mag()), strength(strength) {}
-	// inline Spring(bmath::vec2& p0, bmath::vec2& p1, const float strength, const float restLength):
-	// 	p0(p0), p1(p1), restLength(restLength), strength(strength) {}
-	inline void update() {
-		// const float invMass[2] { 1.f / m0, 1.f / m1};
-		// const float invMass[2] { 1.f, 1.f};
-		const bmath::vec2 p0p1 = p1.pos - p0.pos;
-		const bmath::vec2 dir01 = p0p1 / p0p1.mag();
-		const float currentLength = p0p1.mag();
-		const float deviation = currentLength - restLength;
-		float f = deviation * strength;
-		p0.applyForce(f * dir01);
-		p1.applyForce(-f * dir01);
-	}
-};
 
 
 int main() {
 	GDIWindow win(800, 800);
+	gwin = &win;
 
 	LARGE_INTEGER freq;
 	QueryPerformanceFrequency(&freq);
@@ -131,99 +36,206 @@ int main() {
 	LARGE_INTEGER lastTime;
 	QueryPerformanceCounter(&lastTime);
 
-	PointMass p0({3.f, 1.5f}, 1.f / 0.f);
-	PointMass p1({6.f, 3.5f}, .2f);
-	PointMass p2({2.f, 5.5f}, .2f);
-	PointMass p3({4.f, 7.5f}, .2f);
 
-	Triangle t0{ &p0, &p1, &p2 };
-	Triangle t1{ &p2, &p1, &p3 };
-	TriangleConstraint c0(t0);
-	TriangleConstraint c1(t1);
+	bmath::vec<2, double> origin; // pixel coordinates of physics origin
+	double scale = 100; // pixels per meter
 
-	Spring s0(p0, p1, 50.f);
-	Spring s1(p1, p2, 50.f);
-	Spring s2(p2, p0, 50.f);
 
-	Spring s3(p1, p3, 50.f);
-	Spring s4(p2, p3, 50.f);
 
-	// t0.p0()[0] += .3;
 
-	float prevDt = .016f;
-	float dt;
+	points.push_back(PointMass({3.f, 1.5f}, 1. / 0.));
+	size_t base = 1;
+
+	const double mass = 1.f;
+	// points.push_back(PointMass({3.f, 2.5f}, 1. / 0.));
+	points.push_back(PointMass({3.f, 2.5f}, mass));
+	points.push_back(PointMass({6.f, 3.5f}, mass));
+	points.push_back(PointMass({2.f, 5.5f}, mass));
+
+	points.push_back(PointMass({4.f, 7.5f}, mass));
+
+
+	triangles.push_back(TriangleConstraint(base + 0, base + 1, base + 2));
+	triangles.push_back(TriangleConstraint(base + 2, base + 1, base + 3));
+
+	springs.push_back(Spring(0, 1, 550.f));
+	// lines.push_back(LineConstraint(0, 1));
+
+	const double strength = 5.f;
+	// springs.push_back(Spring(base + 0, base + 1, strength));
+	// springs.push_back(Spring(base + 1, base + 2, strength));
+	// springs.push_back(Spring(base + 2, base + 0, strength));
+
+	// springs.push_back(Spring(base + 1, base + 3, strength));
+	// springs.push_back(Spring(base + 2, base + 3, strength));
+
+	const double restLength = 3.f;
+	springs.push_back(Spring(base + 0, base + 1, strength, restLength));
+	// springs.push_back(Spring(base + 1, base + 2, strength, restLength));
+	springs.push_back(Spring(base + 2, base + 0, strength, restLength));
+
+	springs.push_back(Spring(base + 1, base + 3, strength, restLength));
+	springs.push_back(Spring(base + 2, base + 3, strength, restLength));
+
+
+	int pmouseX, pmouseY;
+	int mouseX = win.win.mouseX, mouseY = win.win.mouseY;
+
+	int pscroll;
+	int scroll = win.win.scroll;
+
+
+	constexpr size_t NUM_SUBSTEPS = 100;
+
+	
 	while(!win.shouldClose()) {
+		pmouseX = mouseX;
+		pmouseY = mouseY;
+		mouseX = win.win.mouseX;
+		mouseY = win.win.mouseY;
+
+		pscroll = scroll;
+		scroll = win.win.scroll;
+
+		if(GetAsyncKeyState(VK_MBUTTON) & 0x8000) {
+			origin[0] += mouseX - pmouseX;
+			origin[1] += mouseY - pmouseY;
+		}
+		if((scroll != pscroll) && GetAsyncKeyState(VK_CONTROL) & 0x8000) {
+			const double diff = scroll - pscroll;
+			const double scaleFac = (diff > 0) ? (1.1) : (.9);
+			scale *= scaleFac;
+			if(scale < 5) scale = 5;
+			// if(scroll != pscroll)
+			// std::cout << "Scroll\n";
+		}
+		if(GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
+			points[0].pos[0] = (std::max<double>(0., std::min<double>(mouseX, win.width))  - origin[0]) / scale;
+			points[0].pos[1] = (std::max<double>(0., std::min<double>(mouseY, win.height)) - origin[1]) / scale;
+			// points[0].prevPos[0] = points[0].pos[0];
+			// points[0].prevPos[1] = points[0].pos[1];
+		}
+		points[0].vel *= 0;
+		points[0].force *= 0;
+
+		win.graphics.clear(0xFFFFFFFF);
 
 		LARGE_INTEGER currentTime;
 		QueryPerformanceCounter(&currentTime);
 
 		// dt = (currentTime.QuadPart - lastTime.QuadPart) * 1. / freq.QuadPart;
-		dt = 1.f / 120.f;
+		double dt = 1.f / 120.f;
 		lastTime = currentTime;
 
-		constexpr size_t NUM_SUBSTEPS = 5;
-
-		static float t = 0;
-		t += dt / NUM_SUBSTEPS;
-		t = fmodf(t, 1.f);
-
+		static double t = .5;
 
 		for(size_t i = 0; i < NUM_SUBSTEPS; i++) {
-			t += dt / NUM_SUBSTEPS;
+			t += dt * .2f / NUM_SUBSTEPS;
 			t = fmodf(t, 1.f);
 
-			// t0.p0()[0] = 2.f + fabsf(2.f * t - 1.f);
+			// points[0].pos[0] = 2.f + fabsf(2.f * t - 1.f);
+			// points[0].pos[0] = 2.f + (sinf(2.f * 3.1415926535f * t) + .5f) * 2.f;
 
 
-			p0.calcVel(prevDt);
-			p1.calcVel(prevDt);
-			p2.calcVel(prevDt);
-			p3.calcVel(prevDt);
+			// for(PointMass& p : points)
+			// 	p.calcVel(prevDt);
 
 			// std::cout << "dt: " << dt << "s\n";
-
 			
-			p0.applyForce({0.f, 9.81f});
-			p1.applyForce({0.f, 9.81f});
-			p2.applyForce({0.f, 9.81f});
-			p3.applyForce({0.f, 9.81f});
+
+			// accumulate forces:
+			constexpr float gravity = 9.81f * .5f;
+			for(PointMass& p : points)
+				if(p.mass != 1.f / 0.f) // dont accelerate points with infinite mass
+					p.applyForce({0.f, gravity * p.mass});
+
+			for(PointMass& p : points)
+				if(p.mass < 1.f / 0.f) // dont accelerate points with infinite mass
+					p.applyForce(-p.vel * .15f);
+
+			for(Spring& s : springs)
+				s.update();
 
 
-			s0.update();
-			s1.update();
-			s2.update();
-			s3.update();
-			s4.update();
+			// apply velocity:
+			for(PointMass& p : points)
+				p.update(dt / NUM_SUBSTEPS);
 
 
-			p0.update(dt / NUM_SUBSTEPS);
-			p1.update(dt / NUM_SUBSTEPS);
-			p2.update(dt / NUM_SUBSTEPS);
-			p3.update(dt / NUM_SUBSTEPS);
+			// constraints:
+			for(TriangleConstraint& t : triangles)
+				t.solve(dt / NUM_SUBSTEPS);
+			for(LineConstraint& l : lines)
+				l.solve(dt / NUM_SUBSTEPS);
+
+			// std::cout << "Correcting p0: " << points[0].pos[0] << ", " << points[0].pos[1];
+			for(PointMass& p : points) {
+				// p.pos[0] = std::max<double>(0., std::min<double>(p.pos[0], win.width / 100.));
+				// p.pos[1] = std::max<double>(0., std::min<double>(p.pos[1], win.height / 100.));
+
+				p.pos[1] = std::min<double>(p.pos[1], 8.);
+			}
+			// std::cout << "  After: " << points[0].pos[0] << ", " << points[0].pos[1] << "\n";
+			
 
 
-			c0.solve(dt / NUM_SUBSTEPS);
-			c1.solve(dt / NUM_SUBSTEPS);
+			// calculate velocity:
+			for(PointMass& p : points)
+				p.calcVel(dt / NUM_SUBSTEPS);
 
-			prevDt = dt / NUM_SUBSTEPS;
+			// prevDt = dt / NUM_SUBSTEPS;
 		}
 
-
-		win.graphics.clear(0xFFFFFFFF);
 
 		// for(size_t i = 0; i < 3; i++)
 		// 	win.graphics.line(
 		// 		t0.p[i]->pos[0] * 100, t0.p[i]->pos[1] * 100,
 		// 		t0.p[(i + 1) % 3]->pos[0] * 100, t0.p[(i + 1) % 3]->pos[1] * 100,
 		// 		0xFF000000);
-		win.graphics.line(p0.pos[0] * 100, p0.pos[1] * 100, p1.pos[0] * 100, p1.pos[1] * 100, 0xFF000000);
-		win.graphics.line(p1.pos[0] * 100, p1.pos[1] * 100, p2.pos[0] * 100, p2.pos[1] * 100, 0xFF000000);
-		win.graphics.line(p2.pos[0] * 100, p2.pos[1] * 100, p0.pos[0] * 100, p0.pos[1] * 100, 0xFF000000);
-		win.graphics.line(p1.pos[0] * 100, p1.pos[1] * 100, p3.pos[0] * 100, p3.pos[1] * 100, 0xFF000000);
-		win.graphics.line(p2.pos[0] * 100, p2.pos[1] * 100, p3.pos[0] * 100, p3.pos[1] * 100, 0xFF000000);
+		// win.graphics.line(p0.pos[0] * 100, p0.pos[1] * 100, p1.pos[0] * 100, p1.pos[1] * 100, 0xFF000000);
+		// win.graphics.line(p1.pos[0] * 100, p1.pos[1] * 100, p2.pos[0] * 100, p2.pos[1] * 100, 0xFF000000);
+		// win.graphics.line(p2.pos[0] * 100, p2.pos[1] * 100, p0.pos[0] * 100, p0.pos[1] * 100, 0xFF000000);
+		// win.graphics.line(p1.pos[0] * 100, p1.pos[1] * 100, p3.pos[0] * 100, p3.pos[1] * 100, 0xFF000000);
+		// win.graphics.line(p2.pos[0] * 100, p2.pos[1] * 100, p3.pos[0] * 100, p3.pos[1] * 100, 0xFF000000);
+	
 
-		for(size_t i = 0; i < 3; i++)
-			win.graphics.fillCircle(t0.p[i]->pos[0] * 100, t0.p[i]->pos[1] * 100, 3, 0xFFFF0000);
+		for(const Spring& s : springs)
+			win.graphics.line(
+				origin[0] + points[s.p0].pos[0] * scale,
+				origin[1] + points[s.p0].pos[1] * scale,
+				origin[0] + points[s.p1].pos[0] * scale,
+				origin[1] + points[s.p1].pos[1] * scale,
+				0xFF000000);
+
+		for(const PointMass& p : points)
+			win.graphics.fillCircle(
+				origin[0] + p.pos[0] * scale,
+				origin[1] + p.pos[1] * scale,
+				3,
+				0xFFFF0000);
+
+		// horizontal origin:
+		win.graphics.line(
+			0,
+			origin[1],
+			win.width,
+			origin[1],
+			0xFFFF0000);
+
+		// vertical origin:
+		win.graphics.line(
+			origin[0],
+			0,
+			origin[0],
+			win.height,
+			0xFF00FF00);
+
+		win.graphics.line(
+			0,
+			origin[1] + 8 * scale,
+			win.width,
+			origin[1] + 8 * scale,
+			0xFF0000FF);
 
 		win.updateScreen();
 
--- src/PointMass.hpp
+++ src/PointMass.hpp
@@ -2,35 +2,46 @@
 
 #include "BMath/vector.h"
 
+#include <vector>
+
+
+struct PointMass;
+
+extern std::vector<PointMass> points;
+
 
 struct PointMass {
     float mass;
-    bmath::vec2 prevPos;
-    bmath::vec2 pos;
-    bmath::vec2 vel{}; // recalculated every frame
-    bmath::vec2 force{};
+    bmath::vec<2, double> prevPos;
+    bmath::vec<2, double> pos;
+    bmath::vec<2, double> vel{}; // recalculated every frame
+    bmath::vec<2, double> force{};
 
     inline PointMass():
-        prevPos(), pos(), mass(1.f) { }
+        mass(1.f), prevPos(), pos()  { }
     inline PointMass(const float px, const float py, const float mass = 1.f):
-        prevPos({px, py}), pos({px, py}), mass(mass) { }
-    inline PointMass(const bmath::vec2& pos, const float mass = 1.f):
-        prevPos(pos), pos(pos), mass(mass) { }
+        mass(mass), prevPos({px, py}), pos({px, py}) { }
+    inline PointMass(const bmath::vec<2, double>& pos, const float mass = 1.f):
+        mass(mass), prevPos(pos), pos(pos) { }
 
     inline void calcVel(const float dt) { // calculates velocity based on prevPos, pos and dt, and resets force
         vel = (pos - prevPos) / dt;
         force = {};
     }
-    void applyForce(const bmath::vec2& force) {
+    inline void applyForce(const bmath::vec<2, double>& force) {
         this->force += force;
     }
     inline void update(const float dt) { // moves particle based on vel and dt
         // integrate acceleration:
-        const bmath::vec2 accel = force / mass;
+        const bmath::vec<2, double> accel = force / mass;
         vel += accel * dt;
     
         // integrate velocity:
         prevPos =  pos;
         pos += vel * dt;
     }
+
+	// inline size_t id() const {
+	// 	return this - points.data();
+	// }
 };
